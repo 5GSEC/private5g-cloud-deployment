d413b798ce4c449203988fa01e45e60717614609
Thiago Navarro <navarro.ime@gmail.com>
2024-07-13 23:04:47 -0300

    adds ecr repository for webui

--- app-cdk/app_cdk/ecr_cdk_stack.py
+++ app-cdk/app_cdk/ecr_cdk_stack.py
@@ -10,13 +10,12 @@ from aws_cdk import (
 class EcrCdkStack(Stack):
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
-        
-        ecr_repository = ecr.Repository(self, 'my-open5gs', removal_policy=RemovalPolicy.DESTROY)
 
+        ecr_repository = ecr.Repository(self, 'my-open5gs', removal_policy=RemovalPolicy.DESTROY)
         ssm.StringParameter(self, "SSMEcrRepositoryUri", parameter_name="EcrRepositoryUri", string_value=ecr_repository.repository_uri)
 
-        CfnOutput(
-            self,
-            "EcrRepositoryUriOutput",
-            value=ecr_repository.repository_uri
-        )
+        web_repository = ecr.Repository(self, 'open5gs-x86-web', removal_policy=RemovalPolicy.DESTROY)
+        ssm.StringParameter(self, "SSMEcrWebRepositoryUri", parameter_name="EcrWebRepositoryUri", string_value=web_repository.repository_uri)
+
+        CfnOutput(self, "EcrRepositoryUriOutput", value=ecr_repository.repository_uri)
+        CfnOutput(self, "EcrWebRepositoryUriOutput", value=web_repository.repository_uri)
